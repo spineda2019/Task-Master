6cd7b31295293ee9a7c0eb44855198038f5f60f2
spineda2019 <spineda@wpi.edu>
2024-01-12 08:43:23 -0500

    did the hello world ui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    MainWindow::new().unwrap().run().unwrap();
+}
+
+slint::slint! {
+
+    export component MainWindow inherits Window {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
 }
